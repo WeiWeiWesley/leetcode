17d18d44caf2cc61d09d65b40931faf4ff5cf84f
wesley <wesley_su@mail.chungyo.net>
2022-06-10 11:49:56 +0800

    Add 26. Remove Duplicates from Sorted Array

---
+++ removeduplicates/removeduplicates.go
@@ -0,0 +1,14 @@
+package removeduplicates
+
+func removeDuplicates(nums []int) int {
+	index := 0
+
+	for i := 1; i < len(nums); i++ {
+		if nums[index] != nums[i] {
+			index++
+			nums[index] = nums[i]
+		}
+	}
+
+	return index + 1
+}
