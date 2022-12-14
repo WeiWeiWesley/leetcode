cc006042ff8e2f9d1da9064d7667a1429219bd4f
wesley <wesley_su@mail.chungyo.net>
2022-12-14 11:50:34 +0800

    two_sum hash table solution

--- two_sum/method.go
+++ two_sum/method.go
@@ -1,14 +1,39 @@
 package twosum
 
+//simple loop version
+// func twoSum(nums []int, target int) []int {
+
+// 	for i := range nums {
+// 		for j := range nums {
+// 			if i < j && nums[i]+nums[j] == target {
+// 				return []int{i, j}
+// 			}
+// 		}
+// 	}
+
+// 	return []int{}
+// }
+
+//hash table version
 func twoSum(nums []int, target int) []int {
 
-	for i := range nums {
-		for j := range nums {
-			if i < j && nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
-		}
+	table := map[int]int{}
+
+	for i, n := range nums {
+		table[n] = i
 	}
 
-	return []int{}
+	res := []int{}
+
+	l := len(nums)
+
+	for i := 0; i < l; i++ {
+
+		if j, ok := table[target-nums[i]]; ok && j != i {
+			return []int{i, j}
+		}
+		
+	}
+
+	return res
 }
