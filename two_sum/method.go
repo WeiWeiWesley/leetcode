92185206394a5e85cc197abc07318126efeb194b
wesley <wesley_su@mail.chungyo.net>
2023-05-04 09:15:13 +0800

    [change] 雙指標解法

--- two_sum/method.go
+++ two_sum/method.go
@@ -15,25 +15,49 @@ package twosum
 // }
 
 //hash table version
+// func twoSum(nums []int, target int) []int {
+
+// 	table := map[int]int{}
+
+// 	for i, n := range nums {
+// 		table[n] = i
+// 	}
+
+// 	res := []int{}
+
+// 	l := len(nums)
+
+// 	for i := 0; i < l; i++ {
+
+// 		if j, ok := table[target-nums[i]]; ok && j != i {
+// 			return []int{i, j}
+// 		}
+
+// 	}
+
+// 	return res
+// }
+
+//雙指標解法
+//左右依序向內縮，達到不重複
 func twoSum(nums []int, target int) []int {
 
-	table := map[int]int{}
+	n := len(nums)
 
-	for i, n := range nums {
-		table[n] = i
-	}
+	//左
+	for l := 0; l < n; l++ {
+		//右
+		for r := n - 1; r > l; r-- {
 
-	res := []int{}
+			sum := nums[l]+nums[r]
 
-	l := len(nums)
+			//條件目標
+			if sum == target {
+				return []int{l, r}
+			}
 
-	for i := 0; i < l; i++ {
-
-		if j, ok := table[target-nums[i]]; ok && j != i {
-			return []int{i, j}
 		}
-		
 	}
 
-	return res
+	return []int{}
 }
