c552c37214848a1e76276d904836913285758c73
wesley <wesley_su@mail.chungyo.net>
2023-05-15 21:32:17 +0800

    [change] 更新解法

--- two_sum/method.go
+++ two_sum/method.go
@@ -15,49 +15,49 @@ package twosum
 // }
 
 //hash table version
-// func twoSum(nums []int, target int) []int {
+func twoSum(nums []int, target int) []int {
 
-// 	table := map[int]int{}
+	table := map[int]int{}
 
-// 	for i, n := range nums {
-// 		table[n] = i
-// 	}
+	for i, n := range nums {
+		table[n] = i
+	}
 
-// 	res := []int{}
+	res := []int{}
 
-// 	l := len(nums)
+	l := len(nums)
 
-// 	for i := 0; i < l; i++ {
+	for i := 0; i < l; i++ {
 
-// 		if j, ok := table[target-nums[i]]; ok && j != i {
-// 			return []int{i, j}
-// 		}
+		if j, ok := table[target-nums[i]]; ok && j != i {
+			return []int{i, j}
+		}
 
-// 	}
+	}
 
-// 	return res
-// }
+	return res
+}
 
 //雙指標解法
 //左右依序向內縮，達到不重複
-func twoSum(nums []int, target int) []int {
+// func twoSum(nums []int, target int) []int {
 
-	n := len(nums)
+// 	n := len(nums)
 
-	//左
-	for l := 0; l < n; l++ {
-		//右
-		for r := n - 1; r > l; r-- {
+// 	//左
+// 	for l := 0; l < n; l++ {
+// 		//右
+// 		for r := n - 1; r > l; r-- {
 
-			sum := nums[l]+nums[r]
+// 			sum := nums[l]+nums[r]
 
-			//條件目標
-			if sum == target {
-				return []int{l, r}
-			}
+// 			//條件目標
+// 			if sum == target {
+// 				return []int{l, r}
+// 			}
 
-		}
-	}
+// 		}
+// 	}
 
-	return []int{}
-}
+// 	return []int{}
+// }
