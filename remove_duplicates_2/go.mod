fb77ed3b15c50eddab3a8604d4cfd843e79e475e
wesley <wesley_su@mail.chungyo.net>
2023-06-05 11:41:33 +0800

    [add] 80. Remove Duplicates from Sorted Array II

---
+++ remove_duplicates_2/go.mod
@@ -0,0 +1,3 @@
+module removeduplicates2
+
+go 1.19
---
+++ remove_duplicates_2/method.go
@@ -0,0 +1,27 @@
+package removeduplicates2
+
+func removeDuplicates(nums []int) int {
+	// 檢查輸入陣列是否為空
+	switch len(nums) {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	}
+
+	// 定義兩個指針：slow 和 fast
+	slow := 2 // slow 指向下一個要填入元素的位置，初始化為 2
+	fast := 2 // fast 遍歷整個陣列，從索引 2 開始
+
+	// 遍歷整個陣列
+	for fast < len(nums) {
+		// 比較 nums[fast] 和 nums[slow-2] 的值
+		if nums[fast] != nums[slow-2] {
+			nums[slow] = nums[fast] // 如果不相等，表示遇到了一個新的元素，將其放入 slow 指向的位置
+			slow++                  // slow 往後移動一格
+		}
+		fast++ // fast 往後移動一格
+	}
+
+	return slow // 返回 slow 的值，即為去重後的陣列長度
+}
