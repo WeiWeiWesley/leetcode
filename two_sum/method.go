f756116a495089ec70e6aa836dbd74a835158608
wesley <wesleymailbox1@gmail.com>
2025-09-02 10:57:52 +0800

    two sum

--- two_sum/method.go
+++ two_sum/method.go
@@ -1,63 +1,20 @@
 package twosum
 
-//simple loop version
-// func twoSum(nums []int, target int) []int {
-
-// 	for i := range nums {
-// 		for j := range nums {
-// 			if i < j && nums[i]+nums[j] == target {
-// 				return []int{i, j}
-// 			}
-// 		}
-// 	}
-
-// 	return []int{}
-// }
-
-//hash table version
+// hash table version
 func twoSum(nums []int, target int) []int {
-
-	table := map[int]int{}
-
-	for i, n := range nums {
-		table[n] = i
-	}
-
-	res := []int{}
-
-	l := len(nums)
-
-	for i := 0; i < l; i++ {
-
-		if j, ok := table[target-nums[i]]; ok && j != i {
-			return []int{i, j}
+	// 定位
+	hashMap := map[int]int{}
+	for i, v := range nums {
+		// 檢查當前數字是否為之前某個數字的補數
+		if index, ok := hashMap[v]; ok {
+			return  []int{index, i}
 		}
 
+		// 記錄當前數字對應的補數索引（如果還沒記錄過）
+		if _, ok := hashMap[target-v]; !ok {
+			hashMap[target-v] = i
+		}
 	}
 
-	return res
+	return []int{}
 }
-
-//雙指標解法
-//左右依序向內縮，達到不重複
-// func twoSum(nums []int, target int) []int {
-
-// 	n := len(nums)
-
-// 	//左
-// 	for l := 0; l < n; l++ {
-// 		//右
-// 		for r := n - 1; r > l; r-- {
-
-// 			sum := nums[l]+nums[r]
-
-// 			//條件目標
-// 			if sum == target {
-// 				return []int{l, r}
-// 			}
-
-// 		}
-// 	}
-
-// 	return []int{}
-// }
