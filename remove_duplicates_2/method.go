bd9062de7d282a0089898f102fe8253734f6ae92
wesley <wesleymailbox1@gmail.com>
2025-09-08 17:05:32 +0800

    練習

--- remove_duplicates_2/method.go
+++ remove_duplicates_2/method.go
@@ -11,16 +11,14 @@ func removeDuplicates(nums []int) int {
 
 	// 定義兩個指針：slow 和 fast
 	slow := 2 // slow 指向下一個要填入元素的位置，初始化為 2
-	fast := 2 // fast 遍歷整個陣列，從索引 2 開始
 
 	// 遍歷整個陣列
-	for fast < len(nums) {
+	for fast:=2; fast < len(nums); fast++ {
 		// 比較 nums[fast] 和 nums[slow-2] 的值
 		if nums[fast] != nums[slow-2] {
 			nums[slow] = nums[fast] // 如果不相等，表示遇到了一個新的元素，將其放入 slow 指向的位置
 			slow++                  // slow 往後移動一格
 		}
-		fast++ // fast 往後移動一格
 	}
 
 	return slow // 返回 slow 的值，即為去重後的陣列長度
--- remove_duplicates_2/method_test.go
+++ remove_duplicates_2/method_test.go
@@ -11,27 +11,54 @@ func Test_removeDuplicates(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
 		{
-			name: "case1",
+			name: "case1 - 基本测试",
 			args: args{
 				nums: []int{1, 1, 1, 2, 2, 3},
 			},
 			want: 5,
 		},
 		{
-			name: "case2",
+			name: "case2 - 无重复超过2次",
 			args: args{
 				nums: []int{1, 1, 2, 2},
 			},
 			want: 4,
 		},
 		{
-			name: "case3",
+			name: "case3 - 单个元素",
 			args: args{
 				nums: []int{1},
 			},
 			want: 1,
+		},
+		{
+			name: "case4 - 空数组",
+			args: args{
+				nums: []int{},
+			},
+			want: 0,
+		},
+		{
+			name: "case5 - 所有元素相同且超过2个",
+			args: args{
+				nums: []int{1, 1, 1, 1, 1},
+			},
+			want: 2,
+		},
+		{
+			name: "case6 - 复杂情况",
+			args: args{
+				nums: []int{0, 0, 1, 1, 1, 1, 2, 3, 3},
+			},
+			want: 7,
+		},
+		{
+			name: "case7 - 只有两个相同元素",
+			args: args{
+				nums: []int{1, 1},
+			},
+			want: 2,
 		},
 	}
 	for _, tt := range tests {
