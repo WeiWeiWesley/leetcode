8ea1076ba9f3da593d6bd0adf66efb4af7700eaa
wesley <wesleymailbox1@gmail.com>
2025-09-08 09:51:18 +0800

    remove element

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 LeetCode Record
 ===
 
-- Recode my leetcode submission
-- Folder named by problems
-- Each folder has a readme.md that problem's description within
+- 紀錄我的解題方式
+- 資料夾的命名為題目名稱
+- 每個資料夾都有 README.md 說明題目內容
--- remove_element/method_test.go
+++ remove_element/method_test.go
@@ -12,7 +12,54 @@ func Test_removeElement(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "example 1",
+			args: args{
+				nums: []int{3, 2, 2, 3},
+				val:  3,
+			},
+			want: 2,
+		},
+		{
+			name: "example 2",
+			args: args{
+				nums: []int{0, 1, 2, 2, 3, 0, 4, 2},
+				val:  2,
+			},
+			want: 5,
+		},
+		{
+			name: "not found",
+			args: args{
+				nums: []int{-1, 1, 2, 3},
+				val:  8,
+			},
+			want: 4,
+		},
+		{
+			name: "empty",
+			args: args{
+				nums: []int{},
+				val:  0,
+			},
+			want: 0,
+		},
+		{
+			name: "first element",
+			args: args{
+				nums: []int{1},
+				val:  1,
+			},
+			want: 0,
+		},
+		{
+			name: "last element",
+			args: args{
+				nums: []int{1, 2, 3, 4, 5},
+				val:  5,
+			},
+			want: 4,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
